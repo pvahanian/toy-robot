8710270e678be13244426521bd23fe3895f86094
willwearing <willwearing@gmail.com>
2021-02-05 20:37:22 -0700

    finished for day

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,22 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRecoilState } from "recoil";
 //state
-import { commandInput, invokeFunc } from "../store/Atom";
-//utils
-import { splitStringCommand } from "../utils/splitInputCommands";
+import {
+  commandInput,
+  modifiedCommandInput,
+  boardPosition,
+  initError,
+} from "../store/Atom";
+//import constants from staysTheSame
+import * as STAYSTHESAME from "../staysTheSame/index";
+import Console from "./Console";
 
+//Component
 const Input: React.FC = () => {
+  //Set recoil state
   const [command, setCommand] = useRecoilState(commandInput);
-  const [invoke, setInvoke] = useRecoilState(invokeFunc);
+  const [modifiedCommand, setModifiedCommand] = useRecoilState(
+    modifiedCommandInput
+  );
+  const [initErrors, setInitErrors] = useRecoilState(initError);
+
+  //Robot control logic:
+  useEffect(() => {
+    let initInput: string = modifiedCommand[0];
+    //build switch statement that does:
+    switch (initInput) {
+      case "PLACE":
+        setInitErrors("PLACE");
+        break;
+      case "MOVE":
+        setInitErrors("MOVE");
+        break;
+      case "LEFT":
+        setInitErrors("LEFT");
+        break;
+      case "RIGHT":
+        setInitErrors("RIGHT");
+        break;
+      case "REPORT":
+        setInitErrors("REPORT");
+        break;
+      default:
+        setInitErrors(
+          "Invalid PLACE command. Your PLACE command should be 'PLACE X,Y,F'"
+        );
+    }
+  }, [modifiedCommand, setInitErrors]);
 
   // React.MouseEvent<HTMLButtonElement> - I know I need to use this but can't make it work - How do I do it correctly?
+  //Handles the onClick mouse event - Splits input command string into an array of strings. Sets that new array to 'modified command' state
   const onSubmit = (e: any) => {
     e.preventDefault();
-    setInvoke(true);
+    let splitStringCommand = (input: string) => {
+      let a: any = input.split(",");
+      setModifiedCommand(a);
+    };
     splitStringCommand(command);
     e.target.reset();
   };
 
+  //Handles user input commands and sets them to 'command' state
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCommand(e.target.value.toUpperCase());
   };
@@ -33,6 +76,7 @@ const Input: React.FC = () => {
         />
         <button>Go</button>
       </form>
+      <p>{initErrors}</p>
     </div>
   );
 };
